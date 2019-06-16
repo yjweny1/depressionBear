83f1684557daf96903d4300c7e55c77c3e2656e0
JiawenYe <827038228@qq.com>
2019-06-16 20:23:44 +1200

    1. Create Splash screen and make an animation 2. Make the home screen with logo and about 3. Make the sample landing page 4. Create empty question page 5. Connect to the firebase

--- app/src/main/java/com/jiawenYe/depressionbear/MainActivity.java
+++ app/src/main/java/com/jiawenYe/depressionbear/MainActivity.java
@@ -1,7 +1,11 @@
 package com.jiawenYe.depressionbear;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.info_text);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    public void goLanding(View view){
+        Intent goSelfTesT = new Intent(this, LandingActivity.class);
+        startActivity(goSelfTesT);
+        finish();
     }
 }
